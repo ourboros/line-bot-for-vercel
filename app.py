3f918526617f75e5cd703d4c2ede0283acbaa8ce
ourboros <74445400+ourboros@users.noreply.github.com>
2024-12-04 21:04:37 +0800

    callback調整

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-from flask import request
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import logging
+import google.generativeai as genai
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -8,7 +13,15 @@ logging.basicConfig(level=logging.INFO)
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    return "OK", 200
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 @app.route("/")
 def home():
